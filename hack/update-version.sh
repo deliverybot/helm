c73292ff80d4d0f7bd55bc18bbbbb6109f9a621b
Colin Walker <colinwalker270@gmail.com>
2019-09-08 20:37:57 +0000

    chore: Release scripting set image name
    Include the image version when doing release scripting.

---
+++ hack/update-version.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export version=$(cat package.json | jq -r '.version')
+yq --yaml-output '.runs.image = "gcr.io/deliverybot/helm:v'${version}'"' < action.yml > action.yml.b && mv action.yml.b action.yml
