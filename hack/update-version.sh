7eddfb971c3fd39f0a673d5afe172172d678c111
Colin Walker <colinwalker270@gmail.com>
2019-09-21 02:01:02 +0000

    fix: CI pipeline using GitHub actions
    Removed google cloud build in favour of actions. Add tests for helm
    charts with this. Removes pulling from google cloud build which
    is not documented anymore by actions.

--- hack/update-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export version=$(cat package.json | jq -r '.version')
-yq --yaml-output '.runs.image = "gcr.io/deliverybot/helm:v'${version}'"' < action.yml > action.yml.b && mv action.yml.b action.yml
-git add .
